d642b0cf4a3f0fd90a7eabed10c16537080826c5
Michael Yogar <michaelyogar878@gmail.com>
2022-02-03 18:22:08 -0500

    feat: Added search bar with icon

--- app/src/main/java/com/example/essentialweather/MainActivity.kt
+++ app/src/main/java/com/example/essentialweather/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.essentialweather
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
